284559c04a04565a793df947275affd60c2a8017
Bloodfinstudios <bloodfinstudios@gmail.com>
2025-08-23 19:44:08 -0600

    This is a test.

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
 from stats import nums_of_word
 from stats import count_char
 from stats import sort_list
+import sys
 
+def get_book_text(path):
+    with open(path) as f:
+        content = f.read()
+        return content
 def main():
-    # This is the main body of my text
-    def get_book_text():
-        with open("books/frankenstein.txt") as f:
-            content = f.read()
-            return content
-    book_text = get_book_text()  
-    return book_text   
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    text = get_book_text(book_path)
 
-book_text = main()  
-word_count = nums_of_word(book_text) 
-char_text = count_char(book_text) 
-true_list = sort_list(char_text)    
+    word_count = nums_of_word(text)
+    char_text = count_char(text)
+    true_list = sort_list(char_text)
 
-print("============ BOOKBOT ============")
-print("Analyzing book found at books/frankenstein.txt...")
-print("----------- Word Count ----------")
-print(f"Found {word_count} total words")
-print("--------- Character Count -------")
-for item in true_list:
-    print(f"{item['char']}: {item['num']}")
-print("============= END ===============")
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...") #
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for item in true_list:
+        if not item["char"].isalpha(): 
+            continue
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 
-main()
 
