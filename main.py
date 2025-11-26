276ca8dcec64e0ad6015137bf94f36c19120ffb1
Bloodfinstudios <bloodfinstudios@gmail.com>
2025-11-25 21:47:09 -0700

    prints book to console

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+path_to_file = "books/frankenstein.txt"
+
+ 
+with open(path_to_file, 'r') as f:
+    content = f.read()
+    
+
+
+def main(content):
+   print(content) 
+
+
+
+main(content)
