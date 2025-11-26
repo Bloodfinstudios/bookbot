d87bbd6132c1657ce00ee68d9ca0bc5905133dd0
Bloodfinstudios <bloodfinstudios@gmail.com>
2025-11-26 16:33:45 -0700

    Counts the words in the book and new file.

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ with open(path_to_file, 'r') as f:
     content = f.read()
     
 
-
 def main(content):
-   print(content) 
+    l = content 
 
-
+def word_counter(content):  
+    words_to_count = content.split()
+    count = len(words_to_count)
+    print(f"Found {count} total words") 
 
 main(content)
+word_counter(content)
