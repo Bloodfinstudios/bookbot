9f2a7771bcf9e512b4802a19c954c64f8f6c7aa8
Bloodfinstudios <bloodfinstudios@gmail.com>
2025-11-27 18:21:07 -0700

    Added some comments

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+
+# main body of the program 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(f"Found {num_words} total words")
 
-
+# gets book from directory
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-
+# finds the number of words
 def get_num_words(text):
     words = text.split()
     return len(words)
